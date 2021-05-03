aa79c342564a36e6160d0d7ac263bef996b3dd69
victor-pg <victor.poalelungi1@gmail.com>
2021-05-03 13:56:59 +0300

    Resolved bug in news section

--- client/src/Components/NewsItem/NewsItem.jsx
+++ client/src/Components/NewsItem/NewsItem.jsx
@@ -4,7 +4,7 @@ import './NewsItem.scss';
 const NewsItem = ({item}) => {
     return(
         <div className="news-item">
-            <div className="news-img-wrapper" style={{backgroundImage:`url(${item.imgurl})`}}></div>
+            <div className="news-img-wrapper" style={{backgroundImage:`url(../../../img/news/${item.imgurl})`}}></div>
             <div className="news-item-title">{item.title}</div>
             <div className="news-item-subtitle">{item.subtitle}</div>
             {/* <div className="news-item-title">{item.timestamp}</div>             */}
